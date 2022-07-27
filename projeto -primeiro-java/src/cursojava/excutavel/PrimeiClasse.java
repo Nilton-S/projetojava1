b7d1ef635f7cdead13618e0193e12819cb942d67
perkles <ferreirasantos45@yahoo.com>
2022-07-27 12:05:45 -0300

    comi

---
+++ projeto -primeiro-java/src/cursojava/excutavel/PrimeiClasse.java
@@ -0,0 +1,12 @@
+package cursojava.excutavel;
+
+public class PrimeiClasse {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("meu primeiro progrma em java");
+		System.out.println("java é orientado a objeto");
+
+	}
+
+}
